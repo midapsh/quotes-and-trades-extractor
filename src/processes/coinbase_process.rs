0b4db88baf194558854439e2bc21840e01a97edc
hspadim <henrique@spadim.com.br>
2022-02-18 18:28:09 -0300

    Set absolute path to coinbase file

--- src/processes/coinbase_process.rs
+++ src/processes/coinbase_process.rs
@@ -5,7 +5,6 @@ use tokio::io::AsyncWriteExt; // for write_all()
 use crate::commands::subscribe::ChannelType;
 use crate::data_extractors::coinbase_websocket::CoinbaseWebsocket;
 
-
 pub async fn coinbase_process() {
     let stream = CoinbaseWebsocket::connect(ChannelType::Level2, vec![String::from("BTC-USD")])
         .await
@@ -16,7 +15,7 @@ pub async fn coinbase_process() {
             let mut file = OpenOptions::new()
                 .append(true)
                 .create(true)
-                .open("coinbase.txt")
+                .open("/home/hspadim/Documents/Rust/exchange-extractor/coinbase.txt")
                 .await?;
             match msg {
                 tokio_tungstenite::tungstenite::Message::Text(message) => {
