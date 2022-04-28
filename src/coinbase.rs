14dc8f736a9720fb671d4168087c76d8bf28fecb
hspadim <henrique@spadim.com.br>
2022-04-28 19:49:19 -0300

    Remove comments

--- src/coinbase.rs
+++ src/coinbase.rs
@@ -8,20 +8,3 @@ use processes::coinbase_process::coinbase_process;
 async fn main() {
     coinbase_process().await;
 }
-
-// use commands::subscribe::{Channel, ChannelType, Subscribe, SubscribeCmd};
-
-// #[tokio::main]
-// async fn main() {
-//     let channel = ChannelType::Level2;
-//     let products_ids = ["BTC-USD"];
-
-//     let subscribe = Subscribe {
-//         _type: SubscribeCmd::Subscribe,
-//         channels: vec![Channel::WithProduct {
-//             channel,
-//             products_ids: products_ids.into_iter().map(|x| x.to_string()).collect(),
-//         }],
-//     };
-//     println!("{}", serde_json::to_string_pretty(&subscribe).unwrap());
-// }
